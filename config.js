c979377d474483b5babf459203939e0466c684c8
MinhQuan08 <95363461+MinhQuan08@users.noreply.github.com>
2021-12-02 14:21:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "CHO TEO HỎI MỤT CÂU",
+    introTitle: 'CHO TEO HỎI MỤT CÂU',
+    introDesc: `CHÚNG MÀY PHẢI NÓI THẬT NHA`,
+    btnIntro: 'OKEE',
+    title: 'MÀY CÓ BIẾT SỦA KHÔNG',
+    desc: 'NÓI THẬT ĐI TEO KHÔNG NÓI AI ĐÂU',
+    btnYes: 'Ừ TAO BIẾT SỦA',
+    btnNo: 'TAO ĐÉO BIẾT SỦA',
+    question: 'CHO TEO BIẾT CÁCH MÀY SỦA VỚI',
+    btnReply: 'GỬI',
+    reply: 'TEO SỦA BẰNG L*N',
+    mess: 'TAO BIẾT NGAY MÀ =))',
+    messDesc: 'TAO BÁO SỞ THÚ BẮT MÀY NÈ :)',
+    btnAccept: 'NGON THÌ BÁO ĐI TAO ĐÉO SỢ =)',
+    messLink: 'https://drkhoe.vn/wp-content/uploads/2020/05/hinh-anh-2-con-cho-12.jpg' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
